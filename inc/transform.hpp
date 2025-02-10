3d0f6047d11459b3ef7822b76c3c55ed6cbb8d2b
LanQiDongLai <i1901433058@163.com>
2025-02-10 10:17:30 +0800

    定义变换接口

---
+++ inc/transform.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "matrix.hpp"
+
+namespace sf {
+namespace Transform {
+
+template <class T>
+Matrix<T, 4> translate(const Matrix<T, 4>& mat, const Vector<T, 4>& vec) {
+  // todo
+  abort();
+}
+template <class T>
+Matrix<T, 4> rotate(const Matrix<T, 4>& mat, float angle,
+                    const Vector<T, 4>& vec) {
+  // todo
+  abort();
+}
+template <class T>
+Matrix<T, 4> scale(const Matrix<float, 4>& mat, const Vector<T, 4>& vec) {
+  // todo
+  abort();
+}
+
+}  // namespace Transform
+}  // namespace sf
